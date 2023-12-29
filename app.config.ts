9f16f4b6e7fd27220fcc8f683a7ebc31cd95232c
lp741 <luca.pulira@gmail.com>
2023-12-29 14:11:17 +0100

    first commit

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Balance Board Pro',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
@@ -11,19 +11,24 @@ export default defineAppConfig({
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        path: '/logo-mini-light.webp', // path of the logo
+        pathDark: '/logo-mini-dark.webp', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'Balanceboard' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: true,
+
+        // our github repository
+        repository: 'https://www.github.com/nuxt-themes/alpine',
+
+        text: 'Balance Board Pro, made in Italy ®'
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: '© 2021-2022 Board Sports Srl - VAT# 12296840965' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
       twitter: 'nuxtlabs',
