8d392fb1f9d580d5e0b171e1439921c7830ee082
lp741 <luca.pulira@gmail.com>
2023-12-29 17:27:30 +0100

    background

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,12 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  alpine: {
+    body: {
+      backgroundColor: {
+        initial: '#f3f5f6',
+        dark: '#0c0c0d'
+      }
+    }
+  }
 })
